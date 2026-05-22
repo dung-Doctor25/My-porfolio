d699d92124c1166839315d32c7a01c542a040bf5
dung-Doctor25 <dungjayson@gmail.com>
2026-05-23 04:39:40 +0700

    Mô tả phần code bạn vừa chỉnh sửa

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv, load_model
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, '.env'))
--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ def skill_detail(request, num):
         lang = 'vi'
 
     # Tự động gọi file HTML tĩnh tương ứng (skill_detail_1.html, skill_detail_2.html...)
-    template_name = f'skill\skill_detail_{num}.html'
+    template_name = f'skill/skill_detail_{num}.html'
     
     return render(request, template_name, {'current_lang': lang})
 
