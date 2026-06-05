e64e718c3e7d2365cdc9c40c80ef6e2ada2c668f
dung-Doctor25 <dungjayson@gmail.com>
2026-06-06 05:34:36 +0700

    cập nhật lại setting file

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -18,9 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.urls import path, include
+from django.conf.urls.static import static
+from Portfolio import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     path("", include("app.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
